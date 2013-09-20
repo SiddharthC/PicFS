adf1681c08f40a21446a378e3da31e32f12b0c41
Siddharth Choudhary <siddharthchoudhary@gatech.edu>
2013-09-20 18:28:27 -0400

    Update

--- assignment1/procFile_v1.c
+++ assignment1/procFile_v1.c
@@ -1,102 +1,130 @@
-/* create a "file" in /proc */
+/* Joe Greubel and Siddharth Choudhary - Key Pair User Lookup System - Sept 19, 2013 */
 
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/proc_fs.h>
 
-#define procfs_name "helloworld"
-#define procfs_userdir "__usernames"
+#define userdir "__usernames"
+#define MAX_USERS 1000
+#define USERNAME_MAX_LENGTH 20
 
-/*structure to hold information about /proc file */
+/* 20 character long usernames + null terminator */
+char usernamelist[MAX_USERS][USERNAME_MAX_LENGTH + 1];
+int num_users = 0;
 
-struct proc_dir_entry *Test_Proc_File;
-struct proc_dir_entry *Username_Proc_File;
+struct proc_dir_entry *procFileList[MAX_USERS];
+struct proc_dir_entry *usernameProcFile;
 
-
-int procfile_read(char *buffer, char **buffer_location, off_t offset, int buffer_lenght, int *eof, void *data)
+int procfileRead(char *buffer, char **buffer_location, off_t offset, int buffer_lenght, int *eof, void *data)
 {
 	int ret;
 
-	printk(KERN_INFO "procfile_read (/proc/%s) called\n", procfs_name);
+	//Get Current User and Get File Name
 
 	if(offset > 0)
 	{
-		ret=0;
+		ret = 0;
 	}
 	else
 	{
+		//Check if Current_User == File_Username
+
 		ret = sprintf(buffer, "HelloWorld!\n");
 	}
+
 	return ret;
 }
 
-int procfile_dir_read(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data)
+int usernamesFileRead(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data)
 {
 
 	int ret;
 
-	printk(KERN_INFO "procfile_read (/proc/%s) called\n", procfs_userdir);
-
 	if(offset > 0)
 	{
-		
-		ret=0;
+		ret = 0;
+	}
+	else
+	{
+		char* usernameBuffer = "";
+		int i;
+
+		for(i=0; i<num_users; i++) {
+			usernameBuffer += username_list[i];
+			usernameBuffer += '\n'; 
+		}
+		ret = sprtinf(buffer, usernameBuffer); 
 	}
 
 	return ret;
 
 }
 
-void username_file_creation(){
+int usernamesFileWrite(struct file *file, const char *buffer, unsigned long count, void *data) {
 
-	username_file_creation();
+	int len = count;
+	char* = writtenData;
 
-	Username_Proc_File = create_proc_entry(procfs_userdir, 0664, NULL);
+	if(count > 8192) len = 8192;
 
-	if (Username_Proc_File == NULL)
+	if(copy_from_user(writtenData, buffer, len))
+		return -EINVAL;
+
+	//SEE WHAT USER WROTE AND ACT ACCORDINGLY
+
+	num_users++;
+	return count;
+}
+
+void keyPairFileCreation(char* username) {
+
+	procFileList[num_users] = create_proc_entry(username, 0444, NULL);
+
+	if(keyFile == NULL)
 	{
-		remove_proc_entry (procfs_userdir, NULL);
-		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", procfs_userdir);
+		remove_proc_entry(username, NULL);
 		return -ENOMEM;
 	}
 
-	add
+	procFileList[num_users]->read_proc = procfs_read;
+	procFileList[num_users]->write_proc = .....
+	procFileList[num_users]->mode = S_IFREG | S_IRUGO;
+	procFileList[num_users]->uid = 0;
+	procFileList[num_users]->gid = 0;
+	procFileList[num_users]->size = 4096;
 
-	Username_Proc_File->read_proc = procfile_dir_read;
-	Username_Proc_File->mode = S_IFREG | S_IRUGO;
-	Username_Proc_File->uid = 0;
-	Username_Proc_File->gid = 0;
-	Username_Proc_File->size = 1024;
+	username_list[num_users] = username;
+}
 
-	printk(KERN_INFO "/proc/%s created\n", procfs_userdir);
+void usernamesFileCreation(){
 
+	usernameProcFile = create_proc_entry(userdir, 0664, NULL);
+
+	if (usernameProcFile == NULL)
+	{
+		remove_proc_entry(userdir, NULL);
+		return -ENOMEM;
+	}
+
+	usernameProcFile->read_proc = usernamesFileRead;
+	usernameProcFile->write_proc = .....
+	usernameProcFile->mode = S_IFREG | S_IRUGO;
+	usernameProcFile->uid = 0;
+	usernameProcFile->gid = 0;
+	usernameProcFile->size = 8192;
 }
 
 int init_module()
 {
-	
-
-	Test_Proc_File = create_proc_entry(procfs_name, 0644, NULL);
-
-	if (Test_Proc_File == NULL)
-	{
-		remove_proc_entry (procfs_name, NULL);
-		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n", procfs_name);
-		return -ENOMEM;
-	}
-
-	Test_Proc_File->read_proc = procfile_read;
-	Test_Proc_File->mode = S_IFREG | S_IRUGO;
-	Test_Proc_File->uid = 0;
-	Test_Proc_File->gid = 0;
-	Test_Proc_File->size = 37;
-
-	printk(KERN_INFO "/proc/%s created\n", procfs_name);
-	return 0;
+	usernamesFileCreation();
 }
 
 void cleanup_module()
 {
-	remove_proc_entry(procfs_name, NULL);
-	printk(KERN_INFO "/proc/%s removed\n", procfs_name);
+	int i;
+	for(i=0; i<num_users; i++) {
+		char* username = username_list[i];
+		remove_proc_entry(username, NULL);
+	}
+	remove_proc_entry(userdir, NULL);
 }
