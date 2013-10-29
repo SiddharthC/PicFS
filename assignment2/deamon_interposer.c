342d921cd088243f069b1b9ba8a3d661e2d08e0f
Siddharth Choudhary <siddharthchoudhary@gatech.edu>
2013-10-29 05:40:37 -0400

    Intermediate deamon done looping problem exists...

--- assignment2/deamon_interposer.c
+++ assignment2/deamon_interposer.c
@@ -13,9 +13,10 @@ int main(int argc, char *argv[]){
 
 	char read_flag=0;
 	FILE *infile=NULL, *outfile=NULL, *log_file=NULL;
-	char tempBuffer[5000000]="";
+	char tempBuffer[4000000]="";
 	char tempBuffer2[4000]="";
 	int i, temp;
+	pid_t pid, sid; 	//process and session id.
 
 	//Before demonizing phase. Read command line parameters for config.
 	if(argc != 3)		//incorrect parameters print error
@@ -33,10 +34,11 @@ int main(int argc, char *argv[]){
 	if(outfile == NULL)
 		exit(EXIT_FAILURE);
 
-	pid_t pid, sid; 	//process and session id.
+	log_file = fopen("/proc/sysmon_log", "r");
+	if(log_file == NULL)
+		exit(EXIT_FAILURE);
 
-	pid = fork();		//fork from parent
-				//NOTE: Should double fork to avoid zombies
+	pid = fork();		//fork from parent				//NOTE: Should double fork to avoid zombies
 	if(pid < 0)
 		exit(EXIT_FAILURE);
 	if(pid > 0)		//Got a good pid so exit the parent
@@ -49,15 +51,11 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE);
 	if((chdir("/")) < 0)
 		exit(EXIT_FAILURE);
-	
-	
 
 	//close standard files not accessible so closed
 	close(STDIN_FILENO);	 
 	close(STDOUT_FILENO);
 	close(STDERR_FILENO);
-
-
 
 //**********************************************************************//
 	while(1){
@@ -65,33 +63,24 @@ int main(int argc, char *argv[]){
 		rewind(infile);
 
 		if(read_flag == '1'){
-
 			fputc('0', infile);
 			rewind(infile);
 			
-			for (i=0; i<1000; i++){
-
-				log_file = fopen("/proc/sysmon_log", "r");
-				if(log_file == NULL)
-					exit(EXIT_FAILURE);
+			for (i=0; i<10; i++){					//Fix for complete buffer change to 1000
 				fread(tempBuffer2, 1, 4000, log_file);
 				strcat(tempBuffer, tempBuffer2);
-				fclose(log_file);
-				if(tempBuffer2[0] == '\0')
-					break;
+				rewind(log_file);
+//				if(tempBuffer2[0] == '\0')
+//					break;
 				memset(tempBuffer2, 0, 4000);
 			}
 			fwrite(tempBuffer, 1, 5000000, outfile);
-
 			memset(tempBuffer, 0, 5000000);
-			printf("PRINTED!\n");
-			
 		}
-
 		sleep(5);
-
 	}
 
+	fclose(log_file);
 	fclose(infile);
 	fclose(outfile);
 	exit(EXIT_SUCCESS);
