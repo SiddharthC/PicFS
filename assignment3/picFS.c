d0a525bff9ef4cd84469079be69041cbfa3c3bb2
Siddharth Choudhary <siddharthchoudhary.ri@gmail.com>
2013-11-21 16:45:02 -0500

    Error fixed

--- assignment3/picFS.c
+++ assignment3/picFS.c
@@ -90,22 +90,24 @@ void database_initializer(void){
 	MYSQL *con = mysql_init(NULL);
 
 	if(con == NULL){
-		fprintf(stderr, "%s\n", mysql_error(con));
+		fprintf(stdout, "%s\n", mysql_error(con));
 		exit(1);
 	}
 
-	if(mysql_real_connect(con, "192.168.5.1", "root", "fire", NULL, 0, NULL, 0) == NULL){
-		fprintf(stderr, "%s\n", mysql_error(con));
+	if(mysql_real_connect(con, "192.168.5.2", "root", "fire", NULL, 0, NULL, 0) == NULL){
+		fprintf(stdout, "%s\n", mysql_error(con));
 		mysql_close(con);
 		exit(1);
 	}
 
-	if (mysql_query(con, "CREATE DATABASE testdb")){
-		fprintf(stderr, "%s\n", mysql_error(con));
+	if (mysql_query(con, "CREATE DATABASE newdb2")){
+		fprintf(stdout, "%s\n", mysql_error(con));
 		mysql_close(con);
 		exit(1);
 	}
-	
+
+	printf("Got here");
+
 	mysql_close(con);
 //	exit(0);
 }
